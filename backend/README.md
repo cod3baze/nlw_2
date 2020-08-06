5bcb0501bb8f28338d2ddaf89a731ab2fbfdf07f
Elias al3x <cod3er4@gmail.com>
2020-08-06 05:35:23 -0300

    Feat: BCKEND closed

---
+++ backend/README.md
@@ -0,0 +1,14 @@
+# Funcionalidades
+
+> migrations: controlam a versão do banco de dados
+
+## conexões
+
+- Rota para listar o total de conexões realizadas;
+- Rota para criar uma nova conexão;
+
+## Aulas
+
+- Rota para criar Aulas;
+- Rota para listar aulas;
+  - Filtrar por matéria, dia da semana e horário;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,12 @@
 import express from "express";
 import cors from "cors";
+import routes from "./routes";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-app.use(cors({ origin: "http://localhost" }));
-
-app.get("/", (request, response) => {
-  return response.json({ message: "Ola mmundo!" });
-});
+app.use(routes);
 
 app.listen(process.env.PORT || 3333, () => {
   console.log("SERVER IS RUNNING IN PORT:" + 3333);
